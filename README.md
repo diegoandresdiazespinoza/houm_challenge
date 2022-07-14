30f290579b34696b4b47fda30f37241a85603fbc
Diego Diaz <danddiaze@falabella.cl>
2022-07-14 01:33:32 -0400

    [ADD] start README. Add try and except at any event

--- README.md
+++ README.md
@@ -1,12 +1,63 @@
-#Solución: Houm Challenge
+#Documentación: Solución a [Houm Challenge](houm_challenge.md)
 
 ##Diseño
+######Supuestos 
+- El dispositivo móvil del Houmer genera las coordenadas (latitud, longitud) y una fecha con tiempo que envía cada cierto tiempo a la API. La fecha se genera en el dispositivo.
+- El Houmer debe existir para enviar una posición.
+- Una propiedad (real state) queda determinada unívocamente por sus coordenadas (latitud, longitud). No existen dos propiedades con las mismas coordenadas.
+- Cuando un Houmer visita una propiedad, indica mediante el móvil que inicia su visita y al terminar indica el término. Con esto se generan los datos para ser enviados a la API; fecha de inicio y fecha de término se generan en el móvil. Mientras el Houmer está visitando una propiedad no se envían datos de su posición (sólo por simplicidad para hacer los cálculos de velocidad y tiempos entre momentos más simples).
+- Por simplicidad no se considera el caso que un Houmer pueda visitar más de una propiedad en una misma ventana de tiempo.
+- Por simplicidad no se envían dos posiciones de un Houmer iguales para tiempos iguales. Es decir, las posiciones se envían desde el móvil cuando existen cambios de posición.
+- La velocidad está en unidades de Km/H.
+- Para la distancia entre posiciones se utiliza la función de distancia [Haversine](https://pypi.org/project/haversine/).
+- Para todas las fechas se asume UTC.
+- Un momento se considera como el intervalo de tiempo que transcurre entre dos posiciones distintas del Houmer.
+- Para el cálculo de los momentos se considera que dos momentos son aquellos que están más cercas en términos de tiempo. 
+- Por simplicidad, para el cálculo de las velocidades se consideran sólo las posiciones (no las visitas a las propiedades).
+![Diagrama para algunos supuestos](./drawings/supuestos.png)
 
+######Arquitectura
+Se considera una arquitectura simple con un servidor que expone rutas (end-points) y que internamente se conecta con una base de datos.
+El servidor es stateless por lo que no guarda información de sesiones, ni cookies, ni estados particulares de las peticiones mismas; cualquier persistencia
+se hace al modelo de datos expuesto en la base de datos.
+![Diagrama para algunos supuestos](./drawings/arquitectura.png)
+######Tech stack
+- El lenguaje de programación es Python 3.10.
+- Para el servidor se utiliza el web framework Flask.
+- Para la base de datos se utiliza Mysql 8.0.29.
+- Las librerías utilizadas se pueden ver en [requirements](./requirements.txt)
+- Se utiliza un ORM mediante SQLAlchemy; la creación de las tablas se hace automáticamente.
+######Diagrama Entidad-Relación
+Se considera el siguiente diagrama entidad relación para la base de datos.
+![Diagrama entidad relación](./drawings/er.png)
+- Un Houmer puede tener asociadas cero o más posiciones (houmer_position).
+- Una visita está asociada a una propiedad (real_state) y un Houmer en cierto intervalo de tiempo.
+- Una propiedad queda determinada por su latitud y longitud.
+- Un Houmer puede tener cero o más visitas (houmer_visit_real_state).
+######Control de versiones
+Se utiliza GIT con un marco de trabajo [GitFlow](https://www.gitkraken.com/learn/git/git-flow) simplificado:
+- Los nombres de ramas se separan en feature/ release/ (por el momento no existen hotfix)
+- La rama develop es la rama main (por simplicidad).
+- No se utilizaron tags (por simplicidad).
 
+######Estructura de directorios
+La estructura de directorios es la siguiente:
+![Diagrama entidad relación](./drawings/estructura_directorios.png)
+- config.py: mantiene configuración para ambientes: development, testing y productivo.
+- main.py: permite correr el servidor Flask.
+- requirements.txt: archivo con las librerías y sus versiones utilizadas.
+- __init__.py: inicialización de la base de datos, la aplicación flask con sus configuraciones.
+- routes.py: contiene todas las rutas o end-points de la API.
+- models.py: contiene todos los modelos para ser usados como ORM mediante SQLAlchemy.
+- drawings: archivos de imágenes para README.md
+- houm_challenge.md: archivo que contiene en markdown las instrucciones del challenge.
 
 
 ##Base de datos
+Se utiliza base de datos relacional considerando que el problema supone relaciones intrínsecas
+entre los Houmers y las propiedades (visitas y posiciones). 
 
+##Correr en local
 
 ##Seguridad
 
--- app/routes.py
+++ app/routes.py
@@ -67,106 +67,122 @@ def status():
 @token_required
 @validate_schema("HOUMER_SCHEMA")
 def add_houmer():
-    email = request.json["email"]
-    name = request.json['name']
-    houmer = Houmer(name=name, email=email)
-    houmer.save()
-    houmer_id = houmer.id
-    return make_response({"message": f"houmer inserted ok. id:{houmer_id}"}, 200)
-
+    try:
+        email = request.json["email"]
+        name = request.json['name']
+        houmer = Houmer(name=name, email=email)
+        houmer.save()
+        houmer_id = houmer.id
+        return make_response({"message": f"houmer inserted ok. id:{houmer_id}"}, 200)
+    except Exception:
+        return make_response({"message": "Error"}, 400)
 
 @app.route('/houm_challenge/houmer/position/', methods=['POST'])
 @token_required
 @validate_schema("POSITION_SCHEMA")
 def add_houmer_position():
-    houmer_id = request.json['houmer_id']
-    latitude = request.json['latitude']
-    longitude = request.json['longitude']
-    date = None
-    if "date" in request.json:
-        date = request.json['date']
-    houmer_position = HoumerPosition(houmer_id=houmer_id, latitude=latitude, longitude=longitude, date=date)
-    houmer_position.save()
-    houmer_position_id = houmer_position.id
-    return make_response({"message": f"houmer position inserted ok. id:{houmer_position_id}"}, 200)
+    try:
+        houmer_id = request.json['houmer_id']
+        latitude = request.json['latitude']
+        longitude = request.json['longitude']
+        date = None
+        if "date" in request.json:
+            date = request.json['date']
+        houmer_position = HoumerPosition(houmer_id=houmer_id, latitude=latitude, longitude=longitude, date=date)
+        houmer_position.save()
+        houmer_position_id = houmer_position.id
+        return make_response({"message": f"houmer position inserted ok. id:{houmer_position_id}"}, 200)
+    except Exception:
+        return make_response({"message": "Error"}, 400)
 
 
 @app.route('/houm_challenge/houmer/visit/', methods=['POST'])
 @token_required
 @validate_schema("HOUMER_VISIT_REAL_STATE_SCHEMA")
 def add_houmer_visit_real_state():
-    houmer_id = request.json['houmer_id']
-    real_state_id = request.json['real_state_id']
-    start_date = request.json['end_date']
-    end_date = request.json['start_date']
-    houmer_visit_real_state = HoumerVisitRealState(houmer_id=houmer_id, real_state_id=real_state_id,
-                                                   start_date=start_date, end_date=end_date)
-    houmer_visit_real_state.save()
-    houmer_visit_real_state_id = houmer_visit_real_state.id
-    return make_response({"message": f"houmer visit real state inserted ok. id:{houmer_visit_real_state_id}"}, 200)
-
+    try:
+        houmer_id = request.json['houmer_id']
+        real_state_id = request.json['real_state_id']
+        start_date = request.json['end_date']
+        end_date = request.json['start_date']
+        houmer_visit_real_state = HoumerVisitRealState(houmer_id=houmer_id, real_state_id=real_state_id,
+                                                       start_date=start_date, end_date=end_date)
+        houmer_visit_real_state.save()
+        houmer_visit_real_state_id = houmer_visit_real_state.id
+        return make_response({"message": f"houmer visit real state inserted ok. id:{houmer_visit_real_state_id}"}, 200)
+    except Exception:
+        return make_response({"message": "Error"}, 400)
 
 @app.route('/houm_challenge/houmer/visit/coordinates', methods=['POST'])
 @token_required
 @validate_schema("HOUMER_VISIT_COORDINATES_SCHEMA")
 def get_real_state_coordinates_visit():
-    houmer_id = int(request.json["houmer_id"])
-    start_date = datetime.datetime.strptime(request.json["date"], "%Y-%m-%d").replace(hour=0, minute=0, second=0)
-    end_date = datetime.datetime.strptime(request.json["date"], "%Y-%m-%d").replace(hour=23, minute=59, second=59)
-    visits: list[HoumerVisitRealState] = db.session.query(HoumerVisitRealState).filter(
-        HoumerVisitRealState.houmer_id == houmer_id, HoumerVisitRealState.start_date >= start_date,
-        HoumerVisitRealState.start_date <= end_date).all()
-    visit_output = []
-    for visit in visits:
-        real_state: RealState = db.session.query(RealState).filter(RealState.id == visit.real_state_id).first()
-        spent_timedelta = visit.end_date - visit.start_date
-        seconds = spent_timedelta.total_seconds()
-        hours = str(seconds // 3600)
-        minutes = str((seconds % 3600) // 60)
-        seconds = str(seconds % 60)
-        spent_time = f"{hours}:{minutes}:{seconds}"
-        real_state_coordinates = {"latitude": real_state.latitude, "longitude": real_state.longitude,
-                                  "spent_time": spent_time}
-        visit_output.append(real_state_coordinates)
-    return make_response(json.dumps(visit_output), 200)
+    try:
+        houmer_id = int(request.json["houmer_id"])
+        start_date = datetime.datetime.strptime(request.json["date"], "%Y-%m-%d").replace(hour=0, minute=0, second=0)
+        end_date = datetime.datetime.strptime(request.json["date"], "%Y-%m-%d").replace(hour=23, minute=59, second=59)
+        visits: list[HoumerVisitRealState] = db.session.query(HoumerVisitRealState).filter(
+            HoumerVisitRealState.houmer_id == houmer_id, HoumerVisitRealState.start_date >= start_date,
+            HoumerVisitRealState.start_date <= end_date).all()
+        visit_output = []
+        for visit in visits:
+            real_state: RealState = db.session.query(RealState).filter(RealState.id == visit.real_state_id).first()
+            spent_timedelta = visit.end_date - visit.start_date
+            seconds = spent_timedelta.total_seconds()
+            hours = str(seconds // 3600)
+            minutes = str((seconds % 3600) // 60)
+            seconds = str(seconds % 60)
+            spent_time = f"{hours}:{minutes}:{seconds}"
+            real_state_coordinates = {"latitude": real_state.latitude, "longitude": real_state.longitude,
+                                      "spent_time": spent_time}
+            visit_output.append(real_state_coordinates)
+        return make_response(json.dumps(visit_output), 200)
+    except Exception:
+        return make_response({"message": "Error"}, 400)
 
 
 @app.route('/houm_challenge/houmer/exceeded_speed', methods=['POST'])
 @token_required
 @validate_schema("HOUMER_EXCEEDED_SPEED_SCHEMA")
 def get_houmer_exceeded_speed():
-    houmer_id = request.json["houmer_id"]
-    max_speed = float(request.json["max_speed"])
-    start_date = datetime.datetime.strptime(request.json["date"], "%Y-%m-%d").replace(hour=0, minute=0, second=0)
-    end_date = datetime.datetime.strptime(request.json["date"], "%Y-%m-%d").replace(hour=23, minute=59, second=59)
-    moments: list[HoumerPosition] = db.session.query(HoumerPosition).filter(HoumerPosition.houmer_id == houmer_id,
-                                                                            HoumerPosition.date >= start_date,
-                                                                            HoumerPosition.date <= end_date).order_by(
-        HoumerPosition.date).all()
-    index = 0
-    speed_exceeded = []
-    for moment in moments:
-        index += 1
-        if index >= len(moments):
-            break
-        next_moment = moments[index]
-        spent_time = next_moment.date - moment.date
-        distance = haversine.haversine((moment.latitude, moment.longitude),
-                                       (next_moment.latitude, next_moment.longitude))
-        speed = distance / (spent_time.total_seconds() / 3600.0)
-        if speed >= max_speed:
-            speed_exceeded.append(moment.date.strftime("%Y-%m-%d %H:%M:%S"))
-    return make_response(json.dumps(speed_exceeded), 200)
+    try:
+        houmer_id = request.json["houmer_id"]
+        max_speed = float(request.json["max_speed"])
+        start_date = datetime.datetime.strptime(request.json["date"], "%Y-%m-%d").replace(hour=0, minute=0, second=0)
+        end_date = datetime.datetime.strptime(request.json["date"], "%Y-%m-%d").replace(hour=23, minute=59, second=59)
+        moments: list[HoumerPosition] = db.session.query(HoumerPosition).filter(HoumerPosition.houmer_id == houmer_id,
+                                                                                HoumerPosition.date >= start_date,
+                                                                                HoumerPosition.date <= end_date).order_by(
+            HoumerPosition.date).all()
+        index = 0
+        speed_exceeded = []
+        for moment in moments:
+            index += 1
+            if index >= len(moments):
+                break
+            next_moment = moments[index]
+            spent_time = next_moment.date - moment.date
+            distance = haversine.haversine((moment.latitude, moment.longitude),
+                                           (next_moment.latitude, next_moment.longitude))
+            speed = distance / (spent_time.total_seconds() / 3600.0)
+            if speed >= max_speed:
+                speed_exceeded.append(moment.date.strftime("%Y-%m-%d %H:%M:%S"))
+        return make_response(json.dumps(speed_exceeded), 200)
+    except Exception:
+        return make_response({"message": "Error"}, 400)
 
 
 @app.route('/houm_challenge/real_state/', methods=['POST'])
 @token_required
 @validate_schema("REAL_STATE_SCHEMA")
 def add_real_state():
-    name = request.json['name']
-    latitude = request.json['latitude']
-    longitude = request.json['longitude']
-    real_state = RealState(name=name, latitude=latitude, longitude=longitude)
-    real_state.save()
-    real_state_id = real_state.id
-    return make_response({"message": f"real state inserted ok. id:{real_state_id}"}, 200)
+    try:
+        name = request.json['name']
+        latitude = request.json['latitude']
+        longitude = request.json['longitude']
+        real_state = RealState(name=name, latitude=latitude, longitude=longitude)
+        real_state.save()
+        real_state_id = real_state.id
+        return make_response({"message": f"real state inserted ok. id:{real_state_id}"}, 200)
+    except Exception:
+        return make_response({"message": "Error"}, 400)
